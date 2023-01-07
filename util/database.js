402c9114cb80304d46bf04ce2d7f02d7925ab738
Saksham Patro <spatro@ekcs.com>
2023-01-07 21:57:58 +0530

    sequelize models association magic func implemented

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'Welcome@342'
+const sequelize = new Sequelize('node-complete', 'root', 'Welcome@342', {
+  dialect: 'mysql',
+  host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
